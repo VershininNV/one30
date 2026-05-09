df23ac350c203f90ddbb7a3a75f6ea2493717e19
Nikita Vershinin <nv9779@mail.ru>
2026-05-09 22:21:20 +0300

    Проверить корректное выполнение Github Action
    Signed-off-by: Nikita Vershinin <nv9779@mail.ru>

--- application-for-deploy/src/app/app.ts
+++ application-for-deploy/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('one30');
+  protected readonly title = signal('1:30');
 }
